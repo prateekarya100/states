d77c91265d622a2ce33a42ef7391c8d81997feb8
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2025-01-07 12:18:58 +0530

    commit tailwindcss states

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*/*.{html,js}"],
+  theme: {
+    extend: {
+      width:{
+        // "max-w-512":"512px",
+        // "max-w-768":"768px"
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
